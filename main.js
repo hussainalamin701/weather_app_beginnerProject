c99f520945e3be8ff2ac3cf28c19dcd0a01e041d
hussain.a121145 <140621473+hussainalamin701@users.noreply.github.com>
2023-09-25 14:17:01 +0100

    Added the rest of the required JS and finished following the tutorial project

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const apiKey = 'a9cf9a11f74e925f7f32b802da107d0f';
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=';
 
-let citySearch = document.querySelector('.search input');
-let cityButton = document.querySelector('.search button');
+const citySearch = document.querySelector('.search input');
+const cityButton = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon');
 
 async function checkWeather(cityname){
     const response = await fetch(apiURL + cityname + `&appid=${apiKey}`);
@@ -14,6 +15,24 @@ async function checkWeather(cityname){
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + ' km/h';
+
+    if(data.weather[0].main == 'Clouds'){
+        weatherIcon.src = 'images/clouds.png';
+    } else if(data.weather[0].main == 'Clear'){
+        weatherIcon.src = 'images/clear.png';
+    }else if(data.weather[0].main == 'Rain'){
+        weatherIcon.src = 'images/rain.png';
+    }else if(data.weather[0].main == 'Drizzle'){
+        weatherIcon.src = 'images/drizzle.png';
+    }else if(data.weather[0].main == 'Mist'){
+        weatherIcon.src = 'images/mist.png';
+    }
+
+    document.querySelector('.weather').style.display = 'block';
 }
 
-checkWeather();
+cityButton.addEventListener('click', ()=>{
+    checkWeather(citySearch.value);
+});
+
+
